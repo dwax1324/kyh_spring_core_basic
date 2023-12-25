b3882c25b23d4c10ca9fd4b6b28da3c1050cd048
dwax1324 <dwax1324@gmail.com>
2023-12-26 00:10:46 +0900

    feat: memberApp, findMember Test

---
+++ src/main/java/kyh/springCoreBasic/MemberApp.java
@@ -0,0 +1,18 @@
+package kyh.springCoreBasic;
+
+import kyh.springCoreBasic.member.Grade;
+import kyh.springCoreBasic.member.Member;
+import kyh.springCoreBasic.member.MemberService;
+import kyh.springCoreBasic.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member1 = new Member(1L, "woojong", Grade.BASIC);
+        memberService.join(member1);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("newMember = " + member1.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
