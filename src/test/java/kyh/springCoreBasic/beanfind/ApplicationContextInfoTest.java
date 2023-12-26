0fb46384fa21a5f92c50acfad395f6ebc4427970
dwax1324 <dwax1324@gmail.com>
2023-12-27 00:57:24 +0900

    learn: 모든 빈 출력하기, 필요한 빈만 출력하기

---
+++ src/test/java/kyh/springCoreBasic/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,36 @@
+package kyh.springCoreBasic.beanfind;
+
+import kyh.springCoreBasic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.print("beanDefinitionName = " + beanDefinitionName + ", ");
+            System.out.println("bean = " + bean);
+        }
+    }
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) { // 내가 등록한 빈만 출력
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.print("beanDefinitionName = " + beanDefinitionName + ", ");
+                System.out.println("bean = " + bean);
+            }
+        }
+    }
+}
